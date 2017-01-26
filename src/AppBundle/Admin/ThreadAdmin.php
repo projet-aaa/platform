be768bd2873341309abb303a6101ab9bb823ff3b
stoakes <Stoakes@users.noreply.github.com>
2017-01-26 19:43:18 +0100

    Add admin for thread

---
+++ src/AppBundle/Admin/ThreadAdmin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ThreadAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('title')
+        ->add('text')
+        ->add('commentaries')
+        ->add('session')
+        ->add('author');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('session')
+        ->add('author');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id')
+            ->add('title')
+            ->add('text')
+            ->add('session')
+            ->add('author');
+    }
+}
