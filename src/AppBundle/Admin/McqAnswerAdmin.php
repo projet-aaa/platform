ed766050f9d6526fd71f16f918aaabc158f0cf33
stoakes <Stoakes@users.noreply.github.com>
2017-01-26 18:51:19 +0100

    Ad McqAnswerAdmin

---
+++ src/AppBundle/Admin/McqAnswerAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class McqAnswerAdmin extends AbstractAdmin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('mcqChoice')
+            ->add('author');
+    }
+
+    // Fields on which entities can be filtered.
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('author');
+    }
+
+    // Fields that will be displayed in list view
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id')
+            ;
+    }
+}
