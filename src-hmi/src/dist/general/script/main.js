2c13e6b92eaff56ceb485dabf2682f557dfdcb7c
GibsS <emerick.gibson@hotmail.fr>
2017-01-31 16:59:03 +0100

    Ajout des fichiers de distribution

---
+++ src-hmi/src/dist/general/script/main.js
@@ -0,0 +1,88 @@
+$(function () {
+    //BEGIN MENU SIDEBAR
+    $('#sidebar').css('min-height', '100%');
+
+    $(window).bind("load resize", function () {
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse');
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse');
+            $('div.sidebar-collapse').css('height', 'auto');
+        }
+        if($('body').hasClass('sidebar-icons')){
+            $('#menu-toggle').hide();
+        } else{
+            $('#menu-toggle').show();
+        }
+    });
+    //END MENU SIDEBAR
+
+    //BEGIN CHECKBOX & RADIO
+    if($('#demo-checkbox-radio').length <= 0){
+        $('input[type="checkbox"]:not(".switch")').iCheck({
+            checkboxClass: 'icheckbox_minimal-grey',
+            increaseArea: '20%' // optional
+        });
+        $('input[type="radio"]:not(".switch")').iCheck({
+            radioClass: 'iradio_minimal-grey',
+            increaseArea: '20%' // optional
+        });
+    }
+    //END CHECKBOX & RADIO
+
+    //BEGIN TOOTLIP
+    $("[data-toggle='tooltip'], [data-hover='tooltip']").tooltip();
+    //END TOOLTIP
+
+    //BEGIN POPOVER
+    $("[data-toggle='popover'], [data-hover='popover']").popover();
+    //END POPOVER
+
+
+    //BEGIN FULL SCREEN
+    $('.btn-fullscreen').click(function() {
+
+        if (!document.fullscreenElement &&    // alternative standard method
+            !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        }
+    });
+    //END FULL SCREEN
+
+
+    //BEGIN BACK TO TOP
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 200) {
+            $('#totop') .fadeOut();
+        } else {
+            $('#totop') .fadeIn();
+        }
+    });
+    $('#totop').on('click', function(){
+        $('html, body').animate({scrollTop:0}, 'fast');
+        return false;
+    });
+    //END BACK TO TOP
+
+});
+
+
+
