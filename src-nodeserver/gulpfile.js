b8c959e96f6852748641b90249e86d1b540bbeaf
GibsS <emerick.gibson@hotmail.fr>
2017-02-01 20:27:20 +0100

    Added build for process for web socket server

--- src-nodeserver/gulpfile.js
+++ src-nodeserver/gulpfile.js
@@ -3,3 +3,24 @@
 var gulp        = require('gulp'),
     typescript  = require('typescript'),
     ts          = require('gulp-typescript'),
+    del         = require('del')
+
+var project = ts.createProject('src/tsconfig.json', { typescript: typescript });
+
+gulp.task('build', function () {
+  var result = gulp.src('src/**/*ts')
+    .pipe(project())
+  return result.js.pipe(gulp.dest('dist'))
+})
+
+gulp.task('deploy', ['build'], function() {
+    gulp.src('node_modules/**/*')
+        .pipe(gulp.dest('../nodejs/node_modules'))
+
+    return gulp.src('dist/**/*')
+        .pipe(gulp.dest('../nodejs/js'))
+})
+
+gulp.task('clean', function(done) {
+    del(['dist'], done.bind(this))
+})
--- src-nodeserver/src/index.ts
+++ src-nodeserver/src/index.ts
@@ -1,1 +1,3 @@
 import * as io from 'socket.io'
+
+console.log("Ca y est!")
