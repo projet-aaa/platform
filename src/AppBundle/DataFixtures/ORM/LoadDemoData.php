a11f94a8c42fc60fdf433f65b9cf1a2146f93e65
stoakes <Stoakes@users.noreply.github.com>
2017-02-08 15:17:49 +0100

    Augment demo fixtures

--- src/AppBundle/DataFixtures/ORM/LoadDemoData.php
+++ src/AppBundle/DataFixtures/ORM/LoadDemoData.php
@@ -41,12 +41,14 @@ class LoadDemoData extends AbstractFixture implements OrderedFixtureInterface
         $t = new Test();
         $t->setTitle('Intro Objets');
         $t->setSession($session1);
+        $t->setLive(true);
         $manager->persist($t);
 
         $q1 = new Question();
         $q1->setText('Can objects of abstract classes be instantiated ?');
         $q1->setExplication('Abstract class can\'t be instantiated');
         $q1->setTypeAnswer('unique'); //one unique true answer.
+        $q1->setTest($t);
         $manager->persist($q1);
 
         $q1_a = new McqChoice();
@@ -73,6 +75,49 @@ class LoadDemoData extends AbstractFixture implements OrderedFixtureInterface
         $a2->setAuthor($this->getReference('uservachard'));
         $manager->persist($a2);
 
+
+        //question 2
+        $tt = new Test();
+        $tt->setTitle('Final Objets');
+        $tt->setSession($session1);
+        $tt->setLive(false);
+        $manager->persist($tt);
+
+        $q2 = new Question();
+        $q2->setText('Mention two forms of polymorphism');
+        $q2->setTypeAnswer('text'); //one unique true answer.
+        $q2->setTest($tt);
+        $manager->persist($q2);
+
+        $q3 = new Question();
+        $q3->setText('Check the words containing a E');
+        $q3->setTypeAnswer('multiple'); //one unique true answer.
+        $q3->setTest($tt);
+        $manager->persist($q3);
+
+        $q3_a = new McqChoice();
+        $q3_a->setQuestion($q3);
+        $q3_a->setText('Inheritance');
+        $q3_a->setCorrect(true);
+        $manager->persist($q3_a);
+
+        $q3_b = new McqChoice();
+        $q3_b->setQuestion($q3);
+        $q3_b->setText('Interface');
+        $q3_b->setCorrect(true);
+        $manager->persist($q3_b);
+
+        $q3_c = new McqChoice();
+        $q3_c->setQuestion($q3);
+        $q3_c->setText('Abstract class');
+        $q3_c->setCorrect(false);
+        $manager->persist($q3_c);
+
+        $q3_d = new McqChoice();
+        $q3_d->setQuestion($q1);
+        $q3_d->setText('Polymorphism');
+        $q3_d->setCorrect(false);
+        $manager->persist($q3_d);
 
 
         $manager->flush();
