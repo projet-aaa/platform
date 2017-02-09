8525b3cd952e545b89ce7c52940defe3e88017f0
quickscop3r <vincent.achard3@gmail.com>
2017-02-09 11:40:07 +0100

    Add the admin for Alert and Feedback

---
+++ src/AppBundle/Admin/FeedbackAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class FeedbackAdmin extends AbstractAdmin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('author')
+            ->add('text')
+            ->add('session')
+            ->add('createdAt');
+    }
+
+    // Fields on which entities can be filtered.
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('author');
+    }
+
+    // Fields that will be displayed in list view
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('id')
+            ->add('author')
+            ->add('text')
+            ->add('session');
+    }
+}
