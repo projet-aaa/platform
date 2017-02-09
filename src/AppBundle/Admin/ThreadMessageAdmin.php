77dd85ee2ab579f2333dc731dd3ab1f45a085e64
stoakes <Stoakes@users.noreply.github.com>
2017-02-09 10:03:57 +0100

    Fix admin for Commentary (renamed ThreadMessage)

--- src/AppBundle/Admin/ThreadMessageAdmin.php
+++ src/AppBundle/Admin/ThreadMessageAdmin.php
@@ -7,7 +7,7 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class CommentaryAdmin extends AbstractAdmin
+class ThreadMessageAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
