7e48e2ce0fc413ffde9b2f0eecf9d6c5a1a99e5b
GibsS <emerick.gibson@hotmail.fr>
2017-02-12 12:07:47 +0100

    Replaced presentation radial by bars

--- src-hmi/src/apps/class/viewTest/testPresentation.tsx
+++ src-hmi/src/apps/class/viewTest/testPresentation.tsx
@@ -14,6 +14,13 @@ viewTestFactory<Props>(View, {
         explanations: ["langage transformé en bytecode", "en effet", "interprété"],
         answer: 1 // index of the right answer (begins at 0)
     },
-    stats: 1, // something not null
+    stats: {
+        "javascript": 10,
+        "C++": 1,
+        "python": 50,
+        "D": 30,
+        "ok": 50,
+        "pas compris": 20
+    }, 
     showCorrection: true
 })
--- src-hmi/src/views/dashboard/presentationView.tsx
+++ src-hmi/src/views/dashboard/presentationView.tsx
@@ -4,23 +4,53 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router"
 import * as MediaQuery from "react-responsive"
+import * as chartjs from "react-chartjs-2"
 
 import { View as QuizView } from "../../views/quiz/quizView"
 import { QuizType, Quiz } from "../../models/class/class"
 
+import { chartColors } from "../../models/consts"
+
 export interface StateProps {
     // a quiz
     quiz: Quiz
-    // statistics
+    // statistics: choice => count
     stats: any
     // true => we show the right answer
     showCorrection: boolean
 }
 export interface ActionProps { }
 
+var maxSize = {
+    height: "100%"
+}
+
 export type Props = StateProps & ActionProps;
 export class View extends React.Component<Props, any> {
     props: Props
+
+    caculateChoiceData(stats): any {
+        let choices = [],
+            count = []
+            
+        for(var k in stats) {
+            choices.push(k)
+            count.push(stats[k])
+        }
+
+        let len = choices.length
+
+        return {
+            labels: choices,
+            datasets: [
+                {
+                    data: count,
+                    backgroundColor: chartColors.slice(0, len),
+                    hoverBackgroundColor: chartColors.slice(0, len)
+                }
+            ]
+        }
+    }
 
     render() {
         const {
@@ -60,9 +90,18 @@ export class View extends React.Component<Props, any> {
                         <div className="col-lg-7">
                             { quizRender }
                         </div>
+                        { showCorrection && 
                         <div className="col-lg-5">
-                            
+                            <div className="panel">
+                                <div className="panel-heading">
+                                    Statistiques:
+                                </div>
+                                <div className="panel-body pan white-background"> 
+                                    <chartjs.Bar data={ this.caculateChoiceData(stats) }/>
+                                </div>
+                            </div>
                         </div>
+                        }
                     </div>
                 </div>
             )
