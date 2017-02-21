93894833928823abaf3090134a50898fec080e92
stoakes <Stoakes@users.noreply.github.com>
2017-02-21 09:59:39 +0100

    Beginning gitlab hook handler

--- src/AppBundle/Controller/GitlabController.php
+++ src/AppBundle/Controller/GitlabController.php
@@ -17,10 +17,41 @@ class GitlabController extends Controller
      *
      * @return Response
      */
-    public function indexAction(Request $request)
+    public function hookAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+        $json = json_decode($request->getContent());
 
-        $response = new JsonResponse(array('status' => 'valid'));
+        if ($json != null && property_exists($json, 'project') && property_exists($json->project, 'http_url')) {
+            $repo = $em->getRepository('AppBundle:Discipline')->findOneByGitUrl($json->project->http_url);
+
+            if ($repo) { //safety property. Should never be false.
+                $rootdir = $this->get('kernel')->getRootDir();
+                $path = $rootdir . '/../var/git/' . $repo->getId();
+                if (is_dir($path)) {
+                    //repo exists, let's pull latest version & update associated quiz
+                    $git_client = $this->get('app.git.client');
+                    $git_repo = $git_client::open($path);
+                    $git_repo->pull();
+                    $response_content = $git_repo->log();
+                    $response_code = 200;
+                    dump($response_content);
+
+                } else {
+                    $response_content = 'No associated folder in var/git';
+                    $response_code = 500;
+                }
+            } else {
+                $response_content = 'No repository linked to that URL :'.$json->project->http_url;
+                $response_code = 404;
+            }
+        } else {
+            $response_content = 'Unreadeable JSON or unknown http_url key';
+            $response_code = 500;
+        }
+
+        $response = new Response($response_content);
+        $response->setStatusCode($response_code);
 
         return $response;
     }
