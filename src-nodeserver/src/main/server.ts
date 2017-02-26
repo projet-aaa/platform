269e956857dfd3a9bbf9d6b50159d334aa5ceb69
stoakes <Stoakes@users.noreply.github.com>
2017-02-26 11:21:24 +0100

    Automatic switch between http and https mode if pem and cert are available

--- src-nodeserver/src/main/server.ts
+++ src-nodeserver/src/main/server.ts
@@ -1,14 +1,30 @@
 import * as http from 'http'
+import * as https from 'https'
 import * as express from 'express'
 import * as socketio from 'socket.io'
 import * as redis from 'redis'
+import * as fs from 'fs'
 
 import { SocketServer } from './socketServer'
 
 import { domain, port, debug } from '../models/consts'
 
+let sslmode_available = fs.existsSync('/tls/server.key') && fs.existsSync('/tls/server.crt');
+
 let app = express()
-let server = http.createServer(app)
+let server = null;
+if(!sslmode_available){
+    server = http.createServer(app)
+    console.log('Server created for http mode (no SSL) ');
+}
+else{
+    let options = {
+        key: fs.readFileSync('/tls/server.key'),
+        cert: fs.readFileSync('/tls/server.crt')
+    };
+    server = https.createServer(options,app);
+    console.log('Server created for https mode (SSL)');
+}
 let io = socketio(server)
 
 let redisClient = redis.createClient(6379, 'redis')
