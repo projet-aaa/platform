1216a0b992c6a37c96f45d92451491d0af20bf71
stoakes <Stoakes@users.noreply.github.com>
2017-02-27 20:14:05 +0100

    Add author automatically for feedback add serialization groups set createdAt auto at creation

--- src/AppBundle/Entity/Feedback.php
+++ src/AppBundle/Entity/Feedback.php
@@ -4,10 +4,17 @@ namespace AppBundle\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 /**
- * @ApiResource
+ * @ApiResource(attributes={
+ *     "normalization_context"={"groups"={"read"}},
+ *     "denormalization_context"={"groups"={"write"}},
+ *     "filters"={"feedback.search"}
+ * })
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
  */
 class Feedback implements \JsonSerializable
 {
@@ -15,28 +22,34 @@ class Feedback implements \JsonSerializable
      * @ORM\Id
      * @ORM\Column(type="guid")
      * @ORM\GeneratedValue(strategy="UUID")
+     * @Groups({"read"})
      */
     private $id;
 
     /**
      * @ORM\ManyToOne(targetEntity="User")
      * @ORM\JoinColumn(name="author_id", referencedColumnName="id")
+     * @Gedmo\Blameable(on="create")
      */
     private $author;
 
     /**
+     * @Assert\NotNull()
      * @ORM\ManyToOne(targetEntity="Session")
+     * @Groups({"read","write"})
      */
     private $session;
 
     /**
      * @ORM\Column(type="datetime", nullable=false)
+     * @Groups({"read"})
      */
     private $createdAt;
 
     /**
      * @Assert\NotBlank()
      * @ORM\Column(type="text", nullable=false)
+     * @Groups({"read","write"})
      */
     private $text;
 
@@ -65,6 +78,13 @@ class Feedback implements \JsonSerializable
     public function __toString()
     {
       return substr($this->getText(), 0, 40).' '.substr($this->getId(),0,5);
+    }
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function prePersist(){
+        $this->createdAt = new \DateTime('now');
     }
 
     /** Auto generated methods*/
