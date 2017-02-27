3e3b828bfcbc324cf6b9f1e2ddcb57db6c6b15c6
stoakes <Stoakes@users.noreply.github.com>
2017-02-27 19:37:29 +0100

    Add doc on Test tree operation

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -10,10 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class TestController extends Controller
 {
     /**
+     * Returns a test and its whole tree (ie questiosn & choices)
      * @param Test $data
      * @return Test
      *
-     * @note this is not an empty function. It returns the test in the tree context, thus with all fields populated.
+     * Note : this is not an empty function. It returns the test in the tree context, thus with all fields populated.
      */
     public function treeAction($data)
     {
