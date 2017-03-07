5a1a462b0d9b802ca7a7bb575747745ec1a14b95
stoakes <Stoakes@users.noreply.github.com>
2017-03-07 17:42:45 +0100

    Remove onurb from modules (should stay in uml) and update dependencies

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -46,7 +46,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Onurb\Bundle\YumlBundle\OnurbYumlBundle();
         }
 
         return $bundles;
